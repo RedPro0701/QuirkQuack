5f3077c969d4b4cc1088f7d6d3452107f37041eb
RedPro0701 <89728008+RedPro0701@users.noreply.github.com>
2023-08-29 21:09:53 +0200

    Update RickRole-and-Wnc.ino

--- ExampleScripts/RickRole-and-Wnc.ino
+++ ExampleScripts/RickRole-and-Wnc.ino
@@ -1,5 +1,7 @@
 //This DigiSpark script opens up Rick Astley's - Never Gonna Give You Up and also a fake Windows update screen and then maximizes it using F11
 #include "DigiKeyboard.h"
+String firsturl = "https://youtu.be/dQw4w9WgXcQ?t=43s";
+String secondurl = "http://fakeupdate.net/wnc";
 void setup() {
   //empty
 }
@@ -8,12 +10,12 @@ void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(600);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
+  DigiKeyboard.print(firsturl);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.print("http://fakeupdate.net/wnc");
+  DigiKeyboard.print(secondurl);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1250);
   DigiKeyboard.sendKeyStroke(KEY_F11);
